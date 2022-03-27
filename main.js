2846bd4893633643b7151fb6ba7b57e4ed40769a
Shreyank01 <87267295+Shreyank01@users.noreply.github.com>
2022-03-27 11:34:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,16 +10,20 @@ function setup() {
     poseNet.on('pose' ,gotPoses);
 }
 
-noseX = "";
-noseY = "";
-left_wristX = "";
-left_wristY = "";
-right_wristX = "";
-right_wristy = "";
+noseX = 0;
+noseY = 0;
+left_wristX = 0;
+diffrence = 0;
+right_wristX = 0;
+
 
 function draw() {
-    background('grey');
-    square(x,y,l);
+    background('white');
+    fill("red");
+    stroke("red");
+    square(noseX,noseY,diffrence);
+
+    document.getElementById("result").innerHTML = "Width and Height of the square is " + diffrence + "px";
 }
 
 function modelLoaded() {
@@ -29,5 +33,15 @@ function modelLoaded() {
 function gotPoses(results) {
         if(results.length > 0) {
             console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("nose x position is " + noseX + "nose y position is" + noseY);
+
+            left_wristX = results[0].pose.leftWrist.x;
+            right_wristX = results[0].pose.rightWrist.x;
+            diffrence = Math.floor(left_wristX - right_wristX);
+            console.log("left wrist x = " + left_wristX + "right wirst x = " + right_wristX + "diffrence = " + diffrence);
+
+            
         }
 }
